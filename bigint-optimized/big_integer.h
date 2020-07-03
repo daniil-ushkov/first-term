286f980e56a2836ea6cbd5d6d5e4329bf7df8ecf
ipythonchik <42135591+ipythonchik@users.noreply.github.com>
2020-07-03 17:25:39 +0300

    bigint-opt (fixed 1)

--- bigint-optimized/big_integer.h
+++ bigint-optimized/big_integer.h
@@ -6,56 +6,9 @@
 #include <algorithm>
 #include <cstring>
 #include <stdexcept>
+#include "buffer.h"
 
 __extension__ typedef unsigned __int128 uint128_t;
-
-struct dynamic_buffer {
-  std::vector<uint32_t> data_;
-  size_t ref_counter;
-
-  explicit dynamic_buffer(size_t size, uint32_t val = 0);
-  dynamic_buffer(dynamic_buffer const& other);
-  dynamic_buffer(uint32_t* static_data_, size_t size);
-};
-
-struct buffer {
-  explicit buffer(size_t size, uint32_t val = 0);
-  buffer(buffer const& other);
-
-  ~buffer();
-
-  uint32_t& operator[](size_t index);
-  uint32_t const& operator[](size_t index) const;
-  uint32_t const& back() const;
-
-  buffer& operator=(buffer const& other);
-
-  void resize(size_t new_size, uint32_t c = 0);
-  void push_back(uint32_t val);
-  void pop_back();
-  void clear();
-//  void reserve(size_t new_capacity);
-
-  size_t size() const;
-
-  bool exclusive() const;
-  bool small() const ;
-
-  bool operator==(buffer const& other) const;
-
- private:
-  void unshare();
-  void alloc_dynamic_data();
-  void realloc_dynamic_data();
-
-  static const size_t MAX_STATIC_SIZE = 2;
-
-  size_t size_;
-  union {
-    dynamic_buffer* dynamic_data_;
-    uint32_t static_data_[MAX_STATIC_SIZE];
-  };
-};
 
 struct big_integer {
   big_integer();
